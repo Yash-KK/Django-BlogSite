3742a2764f4e3ff5573c331d6f5fd1660c88d25f
Yash Kharche <reachyashk@gmail.com>
2022-07-01 14:35:32 +0530

    Django Blog Site

--- blogproj/blog/models.py
+++ blogproj/blog/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 from django.urls import reverse
-
+ 
+   
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -11,7 +12,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.title
+        return f"{self.title}"
 
     def get_absolute_url(self):
         return reverse('blog-detail', kwargs={'pk': self.pk})
